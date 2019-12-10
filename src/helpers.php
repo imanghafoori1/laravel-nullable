5fc09f0fb0a2e1622f2f780aeccceea09fc4f34d
mostafanorzade <mostafanorzade@yahoo.com>
2019-12-10 15:42:01 +0330

    enhance document to helper function

--- src/helpers.php
+++ src/helpers.php
@@ -2,6 +2,15 @@
 
 use Imanghafoori\Helpers\Nullable;
 
+/**
+ * pass object of Nullable
+ *
+ * @param null  $nullable
+ * @param array $message
+ * @param null  $predicate
+ *
+ * @return Nullable
+ */
 function nullable($nullable = null, $message = [], $predicate = null)
 {
     return new Nullable($nullable, (array)$message, $predicate);
