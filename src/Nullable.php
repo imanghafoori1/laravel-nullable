fc79b7950f3b7ca50c844e7acb6c54eefce0f234
mostafanorzade <mostafanorzade@yahoo.com>
2019-12-11 22:25:31 +0330

    edit add php document

--- src/Nullable.php
+++ src/Nullable.php
@@ -2,7 +2,9 @@
 
 namespace Imanghafoori\Helpers;
 
+use Closure;
 use Illuminate\Http\Exceptions\HttpResponseException;
+use InvalidArgumentException;
 use Symfony\Component\HttpFoundation\Response;
 
 class Nullable
@@ -13,20 +15,31 @@ class Nullable
 
     private $message = [];
 
+
+
     /**
      * Nullable constructor.
      *
-     * @param mixed $value
-     * @param array $message
+     * @param mixed    $value
+     * @param array    $message
      * @param callable $predicate
      */
     public function __construct($value = null, array $message = [], $predicate = null)
     {
-        $this->result = $value;
-        $this->message = $message;
+        $this->result    = $value;
+        $this->message   = $message;
         $this->predicate = $predicate;
     }
 
+
+
+    /**
+     * get or
+     *
+     * @param string $default
+     *
+     * @return mixed|null
+     */
     public function getOr($default)
     {
         $p = $this->getPredicate();
@@ -42,6 +55,17 @@ class Nullable
         return $default;
     }
 
+
+
+    /**
+     * pass result or abort error message
+     *
+     * @param int    $code
+     * @param string $message
+     * @param array  $headers
+     *
+     * @return mixed|null
+     */
     public function getOrAbort($code, $message = '', array $headers = [])
     {
         if (!is_null($this->result)) {
@@ -51,6 +75,15 @@ class Nullable
         abort($code, $message, $headers);
     }
 
+
+
+    /**
+     * check valid http response
+     *
+     * @param string $callable
+     *
+     * @return mixed|null
+     */
     public function getOrSend($callable)
     {
         if (!is_null($this->result)) {
@@ -69,9 +102,19 @@ class Nullable
             throw new HttpResponseException($response);
         }
 
-        throw new \InvalidArgumentException('You must provide a valid http response or a callable.');
+        throw new InvalidArgumentException('You must provide a valid http response or a callable.');
     }
 
+
+
+    /**
+     * get param or throw
+     *
+     * @param       $exception
+     * @param mixed ...$parameters
+     *
+     * @return mixed|null
+     */
     public function getOrThrow($exception, ...$parameters)
     {
         if (!is_null($this->result)) {
@@ -81,13 +124,27 @@ class Nullable
         throw_if(true, $exception, ...$parameters);
     }
 
+
+
+    /**
+     * @param callable $callable
+     *
+     * @return mixed
+     */
     public function onValue(callable $callable)
     {
-        if (! is_null($this->result)) {
+        if (!is_null($this->result)) {
             return call_user_func_array($callable, [$this->result]);
         }
     }
 
+
+
+    /**
+     * get predicate
+     *
+     * @return callable|Closure|null
+     */
     private function getPredicate()
     {
         if (is_callable($this->predicate)) {
